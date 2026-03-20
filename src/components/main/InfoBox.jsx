58755f29f0b00f0f1a9d42242b5e6c7355fe9f6f
fatty-turtle <vinhpham06092004@gmail.com>
2026-03-20 18:06:55 +0700

    feat:display all

--- src/components/main/InfoBox.jsx
+++ src/components/main/InfoBox.jsx
@@ -1,11 +1,11 @@
 import LiveClock from "./LiveClock";
 import Compass from "./Compass";
+
 import {
   getTempLabel,
   getWindLabel,
   getVisLabel,
 } from "../../utils/unitConverter.js";
-import { getDisplayValue } from "../../utils/unitUtils.js";
 
 export default function InfoBox({ current, location, isMetric }) {
   const date = location.localtime.slice(0, 10);
@@ -35,95 +35,136 @@ export default function InfoBox({ current, location, isMetric }) {
   const tempDisplay = `${tempPrimary}${isMetric ? tempUnitMetric : tempUnitImperial}`;
 
   return (
-    <section id="current-container" className="flex flex-col gap-5 p-4">
-      <article>
-        <h1 className="text-center m-2.5">CITY: {location.name}</h1>
+    <section id="current-container" className="flex flex-col gap-5 p-4 ">
+      <div id="article">
+        <h1 className="text-center m-2.5">
+          <strong> CITY: {location.name}</strong>
+        </h1>
         <div
           id="city-container"
-          className="flex flex-row justify-evenly gap-2.5 "
+          className="flex flex-row justify-evenly gap-5 "
         >
-          <div
-            id="city-info"
-            className="flex flex-col items-center justify-center"
-          >
-            <p>Time zone: {location.tz_id}</p>
+          <div id="city-info" className="flex flex-col justify-center">
             <p>
-              Date: {date} · {ampm}
+              Time zone: <br></br>
+              <strong>{location.tz_id}</strong>{" "}
+            </p>
+            <p>
+              Date: <br></br>
+              <strong>
+                {date} · {ampm}
+              </strong>
             </p>
           </div>
-          <LiveClock tzId={location.tz_id} size={180}></LiveClock>
+          <LiveClock tzId={location.tz_id} size={150}></LiveClock>
         </div>
-      </article>
-      <article>
-        <h1 className="text-center m-2.5">WEATHER STATUS</h1>
-        <div id="weather-info" className="flex flex-col gap-5">
+      </div>
+      <div className="flex flex-row justify-evenly gap-5">
+        <div id="article">
+          <h1 className="text-center m-2.5">
+            <strong>TEMPERATURE</strong>
+          </h1>
           <div
             id="current-temp-status"
-            className="flex flex-row justify-evenly"
+            className="flex items-center justify-center gap-2.5 flex-row"
           >
             <div>
               <p>
-                Current temperature: <span>({tempDisplay})</span>
+                Temperature: <strong>{tempDisplay}</strong>
               </p>
               <p>
-                Feel like:{" "}
-                {dualDisplay("feelslike_c", "feelslike_f", "°C", "°F")}
+                Feel like:<br></br>
+                <strong>
+                  {dualDisplay("feelslike_c", "feelslike_f", "°C", "°F")}
+                </strong>
               </p>
-              <p>{current.condition.text}</p>
+              <p>
+                Status:
+                <br></br>
+                <strong>{current.condition.text}</strong>
+              </p>
             </div>
             <img src={current.condition.icon} alt="" />
           </div>
+        </div>
+        <div id="article">
+          <h1 className="text-center m-2.5">
+            <strong>WIND</strong>
+          </h1>
           <div
-            id="current-other-status"
-            className="grid grid-cols-2 gap-5 items-center"
+            id="wind"
+            className="flex items-center justify-center gap-2.5 flex-row"
           >
-            <div id="humidity">
-              <p>Humidity: {current.humidity}%</p>
+            <div>
               <p>
-                Dewpoint: {dualDisplay("dewpoint_c", "dewpoint_f", "°C", "°F")}
+                Wind: <br></br>
+                <strong>
+                  {dualDisplay(
+                    "wind_kph",
+                    "wind_mph",
+                    getWindLabel(true),
+                    getWindLabel(false),
+                  )}
+                </strong>
+              </p>
+              <p>
+                Gust: <br></br>
+                <strong>
+                  {dualDisplay(
+                    "gust_kph",
+                    "gust_mph",
+                    getWindLabel(true),
+                    getWindLabel(false),
+                  )}
+                </strong>
+              </p>
+              <p>
+                Direction:<br></br> <strong>{current.wind_dir}</strong>
               </p>
             </div>
-            <div id="wind">
-              <p>
-                Wind:{" "}
-                {dualDisplay(
-                  "wind_kph",
-                  "wind_mph",
-                  getWindLabel(true),
-                  getWindLabel(false),
-                )}
-              </p>
-              <p>
-                Gust:{" "}
-                {dualDisplay(
-                  "gust_kph",
-                  "gust_mph",
-                  getWindLabel(true),
-                  getWindLabel(false),
-                )}
-              </p>
-              <p>Wind direction: {current.wind_dir}</p>
-              <Compass
-                windDegree={current.wind_degree}
-                windDir={current.wind_dir}
-              />
-            </div>
-            <div id="other">
-              <p>
-                Vision:{" "}
-                {dualDisplay(
-                  "vis_km",
-                  "vis_miles",
-                  getVisLabel(true),
-                  getVisLabel(false),
-                )}
-              </p>
-              <p>Cloud: {current.cloud}%</p>
-              <p>UV: {current.uv}</p>
-            </div>
+            <Compass windDegree={current.wind_degree} size={150} />
           </div>
         </div>
-      </article>
+      </div>
+      <div id="article">
+        <h1 className="text-center m-2.5">
+          <strong>WEATHER STATUS</strong>
+        </h1>
+        <div id="other">
+          <p>
+            Humidity:<br></br> <strong>{current.humidity}%</strong>
+          </p>
+          <p>
+            Dewpoint: <br></br>
+            <strong>
+              {dualDisplay("dewpoint_c", "dewpoint_f", "°C", "°F")}
+            </strong>
+          </p>
+          <p>
+            Vision: <br></br>
+            <strong>
+              {dualDisplay(
+                "vis_km",
+                "vis_miles",
+                getVisLabel(true),
+                getVisLabel(false),
+              )}
+            </strong>
+          </p>
+          <p>
+            Heat: <br></br>
+            <strong>
+              {dualDisplay("heatindex_c", "heatindex_f", "°C", "°F")}
+            </strong>
+          </p>
+          <p>
+            Cloud:<br></br> <strong>{current.cloud}%</strong>
+          </p>
+          <p>
+            UV:<br></br> <strong>{current.uv}</strong>
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
